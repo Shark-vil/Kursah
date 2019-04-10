df2ca3047baf618fc2176ab9f944584c073ad4ee
Shark_vil <shark_vil@mail.ru>
2019-04-10 09:52:54 +0300

    Install Nlog.Config

--- Kursah/Kursah/MainWindow.xaml.cs
+++ Kursah/Kursah/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog.Fluent;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace Kursah
         public MainWindow()
         {
             InitializeComponent();
+
+            Log.Info("Application started");
         }
     }
 }
