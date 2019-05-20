ed8de27655694e8c5438b6ff5b177a991daef5be
SanyaTW <1spangls1@gmail.com>
2019-05-20 09:40:03 +0300

    Создал Vm файлы остальных стадий и подключил в MainWindowVM

--- Kursah/Kursah/ViewModel/MainViewVM.cs
+++ Kursah/Kursah/ViewModel/MainViewVM.cs
@@ -16,12 +16,18 @@ namespace Kursah.ViewModel
             Stage_1_1_Context = new Stage_1_1VM();
             Stage_1_2_Context = new Stage_1_2VM();
             Stage_2_Context = new Stage_2VM();
+            Stage_3_Context = new Stage_3VM();
+            Stage_4_Context = new Stage_4VM();
+            FinalizeContext = new FinalizeVM();
         }
 
         public InitializeVM IntroductionContext { get; set; }
         public Stage_1_1VM Stage_1_1_Context { get; set; }
         public Stage_1_2VM Stage_1_2_Context { get; set; }
         public Stage_2VM Stage_2_Context { get; set; }
+        public Stage_3VM Stage_3_Context { get; set; }
+        public Stage_4VM Stage_4_Context { get; set; }
+        public FinalizeVM FinalizeContext { get; set; }
 
         public string Answer { get; set; }
     }
---
+++ Kursah/Kursah/ViewModel/Stage_3VM.cs
@@ -0,0 +1,15 @@
+﻿using Kursah.Common;
+using Kursah.Model;
+
+using BaseMVVM.Command;
+using BaseMVVM.Abstraction;
+
+using System.Collections.Generic;
+using System;
+
+namespace Kursah.ViewModel
+{
+    public class Stage_3VM : ViewModelBase
+    {
+    }
+}
