40f795bbb69303ed950e8d271c004e60cd2ae8de
Shark_vil <shark_vil@mail.ru>
2019-05-20 17:03:25 +0300

    Расчёт БД ценовых показателей - Stage_3

--- Kursah/Kursah/ViewModel/Stage_3VM.cs
+++ Kursah/Kursah/ViewModel/Stage_3VM.cs
@@ -14,13 +14,61 @@ namespace Kursah.ViewModel
         public static List<Stage_3M> Stage_3_Data { get; private set; }
         public SimpleCommand MathTotal { get; set; }
 
+        private string _total;
+        private string _error;
+
+        public string Total
+        {
+            get => _total;
+            set
+            {
+                _total = value;
+
+                OnPropertyChanged();
+            }
+        }
+        public string Error
+        {
+            get => _error;
+            set
+            {
+                _error = value;
+
+                OnPropertyChanged();
+            }
+        }
+
         public Stage_3VM()
         {
             Stage_3_Data = kursahEntities.Instane.Database.SqlQuery<Stage_3M>(Queries.Stage_3Querry).ToListAsync().Result;
 
             MathTotal = new SimpleCommand(() =>
             {
+                if (Stage_3_Data.Count == 0)
+                {
+                    Total = "";
+                    Error = "Нету объектов, которые бы удовлетворяли требованиям вычисления";
+                    return;
+                }
 
+                double summ = 0;
+                int count = InitializeVM.Counts.Count;
+
+                foreach (Stage_3M value in Stage_3_Data.FindAll(item => item.IsSelected))
+                {
+                    summ += value.GoodPrice;
+                }
+
+                if (summ == 0)
+                {
+                    Total = "";
+                    Error = "Не выбран ни один элемент списка";
+                    return;
+                }
+
+                double result = summ / count;
+
+                Total = result.ToString();
             });
         }
     }
